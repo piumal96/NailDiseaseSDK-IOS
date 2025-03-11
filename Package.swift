a1c2f2288c24693960e953249b8cac3f6b176324
licensespring <piumalkumara@live.com>
2025-03-12 02:08:52 +0530

    swift 3.02 -> 6.0 update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -15,6 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NailDiseaseSDK",
-            path: "NailDiseaseSDK.xcframework"  
+            path: "./NailDiseaseSDK.xcframework"
+        ),
     ]
 )
+
